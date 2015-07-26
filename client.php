fcb076f9ebea8ec6f78065280c1e5c7daaf21dc8
thumber-co <support@thumber.co>
2015-07-26 12:16:00 -0500

    Adding support for subscription.json endpoint. Other enhancements to how the client flow works.

--- client.php
+++ client.php
@@ -5,8 +5,7 @@ ThumberClient::init();
 /**
  * Class to process sending requests and receiving responses.
  */
-class ThumberClient
-{
+class ThumberClient {
    /**
     * The Thumber.co API subdomain.
     */
@@ -18,7 +17,12 @@ class ThumberClient
    const ThumberServerCreatePath = '/create.json';
 
    /**
-    * The path for retrieving the supported MIME types.
+    * The path for GETting the user's current subscription info.
+    */
+   const ThumberSubscriptionPath = '/subscription.json';
+
+   /**
+    * The path for GETting the supported MIME types.
     */
    const ThumberServerMimeTypesPath = '/mime_types.json';
 
@@ -44,6 +48,16 @@ class ThumberClient
    private static $thumberClientPath;
 
    /**
+    * @var object The subscription object.
+    */
+   private static $subscription;
+
+   /**
+    * @var array The supported MIME types.
+    */
+   private static $mime_types;
+
+   /**
     * Initialized class members.
     */
    public static function init() {
@@ -52,45 +66,22 @@ class ThumberClient
    }
 
    /**
-    * Sends the provided request to the API endpoint.
+    * Sends the provided request to Thumber for processing.
     *
-    * @param ThumberReq $req The request to be sent. UID, callback, and timestamp
-    * will be written by client. Additionally, nonce will be set if not already set.
-    * @return array containing data about success of the cURL request.
+    * NOTE: Caller should consider testing file size against getSubscription()->file_size_limit to avoid unnecessary
+    * HTTP requests when the file will be rejected.
+    *
+    * @param ThumberThumbReq $req The request to be sent.
+    * @return array The results of sending the request.
     */
-   public function sendRequest($req) {
-      include_once self::$thumberClientPath . 'request.php';
-
-      if (!($req instanceof ThumberReq)) {
-         $this->handleError('Request must be of type ThumberReq.');
+   public function sendThumbRequest($req) {
+      if (!($req instanceof ThumberThumbReq)) {
+         $err = 'Request must be of type ThumberThumbReq.';
+         $this->handleError($err);
+         return array('error' => $err);
       }
 
-      $req->setTimestamp(time());
-
-      $uid = $req->getUid();
-      if (empty($uid)) {
-         $req->setUid(self::$uid);
-      }
-
-      $nonce = $req->getNonce();
-      if (empty($nonce)) {
-         $req->setNonce();
-      }
-
-      $req->setChecksum($req->computeChecksum(self::$userSecret));
-
-      if (!$req->isValid(self::$userSecret)) {
-         $this->handleError('Invalid request provided.');
-      }
-
-      $json = $req->toJson();
-      $url = 'http://' . self::ThumberServerHost . self::ThumberServerCreatePath;
-      $headers = array('Content-Type: application/json', 'Content-Length: ' . strlen($json));
-      $result = $this->sendToThumber('POST', $url, $headers, $json);
-      $result['nonce'] = !array_key_exists('error', $result) ? $req->getNonce() : '';
-
-      // caller should handle errors sensibly
-      return $result;
+      return $this->sendRequest($req, 'POST', self::ThumberServerCreatePath);
    }
 
    /**
@@ -98,18 +89,20 @@ class ThumberClient
     * If not using client.php as the webhook, whoever receives webhook response should first invoke this method to
     * validate response.
     */
-   public function receiveResponse() {
-      include_once self::$thumberClientPath . 'response.php';
+   public function receiveThumbResponse() {
+      include_once self::$thumberClientPath . 'thumb-response.php';
 
       $json = stream_get_contents(fopen('php://input', 'r'));
-      $resp = ThumberResp::parseJson($json);
+      $resp = ThumberThumbResp::parseJson($json);
 
       if (is_null($resp)) {
          $this->handleError('Failed to parse JSON in POST body: ' . $json);
+         return null;
       }
 
       if (!$resp->isValid(self::$userSecret)) {
          $this->handleError('Received invalid response: ' . $json);
+         return null;
       }
 
       // This method should be overridden in order to use response
@@ -117,14 +110,102 @@ class ThumberClient
    }
 
    /**
+    * @return object|null Returns an object with subscription information. Object includes the following:
+    * name, monthly_cost, watermark, direct_upload, thumbs_per_hr_limit, file_size_limit, and thumb_size_limit.
+    */
+   public function getSubscription() {
+      include_once self::$thumberClientPath . 'base-transaction.php';
+
+      if (isset(self::$subscription)) {
+         return self::$subscription;
+      } else {
+         $resp = $this->sendRequest(new ThumberBaseTransaction(), 'GET', self::ThumberSubscriptionPath);
+         if (!array_key_exists('error', $resp)) {
+            self::$subscription = json_decode($resp['body']);
+            return self::$subscription;
+         } else {
+            $this->handleError('Failed to retrieve subscription: ' . $resp['body']);
+            return null;
+         }
+      }
+   }
+
+   /**
     * Retrieves the supported MIME types from Thumber.
     * @return array The supported MIME types reported by the Thumber server.
     */
    public function getMimeTypes() {
-      $headers = array('Content-Type: application/json', 'Content-Length: 0');
-      $url = 'http://' . self::ThumberServerHost . self::ThumberServerMimeTypesPath;
-      $result = $this->sendToThumber('GET', $url, $headers);
-      return !array_key_exists('error', $result) ? json_decode($result['body'], true) : array();
+      if (!isset(self::$mime_types)) {
+         $headers = array('Content-Type: application/json', 'Content-Length: 0');
+         $result = $this->sendToThumber('GET', self::getThumberUrl(self::ThumberServerMimeTypesPath), $headers);
+         self::$mime_types = !array_key_exists('error', $result) ? json_decode($result['body'], true) : array();
+      }
+
+      return self::$mime_types;
+   }
+
+   /**
+    * Sends the provided request to the API endpoint.
+    *
+    * @param ThumberBaseTransaction $req The request to be sent. UID, callback, and timestamp
+    * will be written by client. Additionally, nonce will be set if not already set.
+    * @param string $type The type of HTTP request (GET/POST/etc.)
+    * @param string $path The relative path on Thumber server to contact.
+    * @return array containing data about success of the cURL request.
+    */
+   public function sendRequest($req, $type, $path) {
+      include_once self::$thumberClientPath . 'base-transaction.php';
+
+      if (!is_a($req, 'ThumberBaseTransaction', false)) {
+         $err = 'Request must be of type ThumberBaseTransaction.';
+         $this->handleError($err);
+         return array('error' => $err);
+      }
+
+      // whether we've changed any field, requiring recompute of checksum
+      $changed = false;
+
+      $timestamp = $req->getTimestamp();
+      if (empty($timestamp)) {
+         $req->setTimestamp(time());
+         $changed = true;
+      }
+
+      $uid = $req->getUid();
+      if (empty($uid)) {
+         $req->setUid(self::$uid);
+         $changed = true;
+      }
+
+      $nonce = $req->getNonce();
+      if (empty($nonce)) {
+         $req->setNonce();
+         $changed = true;
+      }
+
+      if ($changed) {
+         $req->setChecksum($req->computeChecksum(self::$userSecret));
+      }
+
+      if (!$req->isValid(self::$userSecret)) {
+         $err = 'Invalid request provided.';
+         $this->handleError($err);
+         return array('error' => $err);
+      }
+
+      $body = '';
+      if ($type == 'GET') {
+         $path .= '?' . $req->toQuery();
+      } else {
+         $body = $req->toJson();
+      }
+
+      $headers = array('Content-Type: application/json', 'Content-Length: ' . strlen($body));
+      $result = $this->sendToThumber($type, self::getThumberUrl($path), $headers, $body);
+      $result['nonce'] = !array_key_exists('error', $result) ? $req->getNonce() : '';
+
+      // caller should handle errors sensibly
+      return $result;
    }
 
    /**
@@ -143,7 +224,7 @@ class ThumberClient
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
       curl_setopt($ch, CURLOPT_URL,            $url);
       curl_setopt($ch, CURLOPT_HTTPHEADER,     $httpHeaders);
-      if ($type == 'POST' && strlen($body) != 0) {
+      if ($type == 'POST') {
          curl_setopt($ch, CURLOPT_POSTFIELDS,  $body);
       }
 
@@ -175,9 +256,17 @@ class ThumberClient
    }
 
    /**
+    * @param string $path The path at Thumber to generate URL from.
+    * @return string The full URL to the given relative path.
+    */
+   private static function getThumberUrl($path) {
+      return 'http://' . self::ThumberServerHost . $path;
+   }
+
+   /**
     * @param $err string Fires on fatal error.
     */
    protected function handleError($err) {
-      die($err);
+      echo $err;
    }
 }
